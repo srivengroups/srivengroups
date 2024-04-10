b94882dbac5bc93cb7ebc69215166d2b4d34f05c
chenji sai teja <chenjisaiteja143@gmail.com>
2024-04-10 08:16:28 +0530

    form update

--- src/Components/KnowMore.jsx
+++ src/Components/KnowMore.jsx
@@ -9,9 +9,9 @@ const KnowMore = () => {
         <section id="contactUs" className="contactSection">
               {/* <MapView coordinates={[12.8912831, 77.5799345]}/> */}
               <iframe title="srivengroups-mapview" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15557.110713692176!2d77.5817269!3d12.889858!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xafec68da8c5a5849%3A0x18c07a54d42d4a09!2sSRIVEN%20GROUPS!5e0!3m2!1sen!2sin!4v1711958668544!5m2!1sen!2sin" width="600" height="450" style={{border: 0, boxShadow: "2px 4px 8px"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            <div data-aos="flip-left" data-aos-delay="150" className="form-Container aos-init aos-animate">
-                <form  name="reachUs" method="POST" >
-                    <input type="hidden" name="form-name" value="reachUS" />
+            <div className="form-Container">
+                <form  name="reach-us" method="POST" >
+                    <input type="hidden" name="form-name" value="reach-us" />
                     <h2>Reach <span style={{WebkitTextFillColor: "#E62249"}}>Us</span></h2>
                     <p>We will be happy to assist you</p>
                     <div className="inputContainer">

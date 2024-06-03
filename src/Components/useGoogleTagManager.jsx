d6290f2d668882ff010ee2c0e4add0de7bc7360f
chenji sai teja <chenjisaiteja143@gmail.com>
2024-06-03 17:17:33 +0530

    sky blossom gtm

--- src/Components/useGoogleTagManager.jsx
+++ src/Components/useGoogleTagManager.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 const useGoogleTagManager = ({id}) => {
   useEffect(() => {
-    const script = document.createElement('script');
+    const script = document.createElement('script') ;
     script.innerHTML = 
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
